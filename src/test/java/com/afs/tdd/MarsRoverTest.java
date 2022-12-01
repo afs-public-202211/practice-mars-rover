09a8fcc25e586a05e47e220a6dc916da24531cec
WONGCH9 <wongch9@oocl.com>
2022-12-01 17:23:50 +0800

    test: added test should_go_to_neg1_0_W_when_execute_command_given_0_0_W_and_M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -47,4 +47,19 @@ class MarsRoverTest {
         assertEquals("E", marsRover.getDirection());
     }
 
+    @Test
+    void should_go_to_neg1_0_W_when_execute_command_given_0_0_W_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0, "W");
+
+        //when
+        marsRover.excuteCommand("M");
+
+        //then
+        assertEquals(-1, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getDirection());
+    }
+
+
 }
