936f0fc84799ed569445e17c9fae29b19ce16592
VINCENT TAM (SYS-TSV-ISD-OOCLL/HKG) <TAMVI@OOCL.COM>
2022-12-01 17:19:54 +0800

    Feat: movefoward Test:00EM -< 10E

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
-    //00NN -< 01N
+    //00NM -< 01N
     @Test
     void should_go_to_0_1_N_when_execute_command_given_0_0_N_and_M() {
         //given
@@ -15,5 +15,15 @@ class MarsRoverTest {
         //then
         assertEquals("0 1 N",marsRover.getStatus());
 
+    }
+    //00EM -< 01E
+    @Test
+    void should_go_to_0_1_N_when_execute_command_given_0_0_E_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0, "E");
+        //when
+        marsRover.executeCommon("M");
+        //then
+        assertEquals("1 0 E",marsRover.getStatus());
     }
 }
