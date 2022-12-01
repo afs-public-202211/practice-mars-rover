250734eadb42d44f86b121348456e34aa84f33fa
VINCENT TAM (SYS-TSV-ISD-OOCLL/HKG) <TAMVI@OOCL.COM>
2022-12-01 17:17:57 +0800

    Feat: movefoward Test:00NM -< 01N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,37 @@
 package com.afs.tdd;
 
 public class MarsRover {
+
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+
+    public MarsRover(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public String getStatus() {
+        return String.format("%s %s %s", this.locationX, this.locationY, this.direction);
+    }
+
+    public void executeCommon(String direction){
+        if (direction == "M"){
+            moveForward();
+    }
+
 }
+
+    private void moveForward() {
+        if (this.direction == "E")
+            this.locationX++;
+        else if (this.direction == "S")
+            this.locationY--;
+        else if (this.direction == "W")
+            this.locationX--;
+        else if (this.direction == "N")
+            this.locationY++;
+    }
+    }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
+    //00NN -< 01N
     @Test
-    void write_your_first_test() {
+    void should_go_to_0_1_N_when_execute_command_given_0_0_N_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0, "N");
+        //when
+        marsRover.executeCommon("M");
+        //then
+        assertEquals("0 1 N",marsRover.getStatus());
 
     }
 }
