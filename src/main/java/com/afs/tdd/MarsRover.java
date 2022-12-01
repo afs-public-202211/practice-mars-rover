ed5d41eb642d28244b243b65f52df707ddd17924
WONGCH9 <wongch9@oocl.com>
2022-12-01 17:11:45 +0800

    feat: added move feature when direction is North

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,24 +2,38 @@ package com.afs.tdd;
 
 public class MarsRover {
 
-    public MarsRover(int locationX, int locationY, String direction){
+    private final int locationX;
+    private int locationY;
+    private final String direction;
 
+    public MarsRover(int locationX, int locationY, String direction){
+        this.locationX= locationX;
+        this.locationY=locationY;
+        this.direction=direction;
     }
 
-    public void excuteCommand(String m){
+    public void excuteCommand(String command){
+        if(command.equals("M")){
+            move();
+        }
+    }
 
+    private void move(){
+        if(direction.equals("N")){
+            locationY++;
+        }
     }
 
 
     public int getLocationX() {
-        return 0;
+        return locationX;
     }
 
     public int getLocationY() {
-        return 0;
+        return locationY;
     }
 
-    public int getDirection() {
-        return 0;
+    public String getDirection() {
+        return direction;
     }
 }
