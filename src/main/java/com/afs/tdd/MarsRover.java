b8c96cf78a67d409b330346395ef3302a5c44065
WONGCH9 <wongch9@oocl.com>
2022-12-01 16:58:38 +0800

    test: added test should_go_to_0_1_N_when_execute_command_given_0_0_N_and_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,25 @@
 package com.afs.tdd;
 
 public class MarsRover {
+
+    public MarsRover(int locationX, int locationY, String direction){
+
+    }
+
+    public void excuteCommand(String m){
+
+    }
+
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public int getDirection() {
+        return 0;
+    }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,25 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
-    void write_your_first_test() {
+    void should_go_to_0_1_N_when_execute_command_given_0_0_N_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0, "N");
+
+        //when
+        marsRover.excuteCommand("M");
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
 
     }
+
+
+
+
 }
